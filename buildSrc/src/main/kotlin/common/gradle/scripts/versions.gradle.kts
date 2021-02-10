e10a00ecf9e0aefeb89645faae156d6f91a24c5e
Juan Ara <juan@juanylaura.es>
2021-02-10 12:46:41 +0100

    Allow to override per submodule

--- buildSrc/src/main/kotlin/common/gradle/scripts/versions.gradle.kts
+++ buildSrc/src/main/kotlin/common/gradle/scripts/versions.gradle.kts
@@ -1,4 +1,9 @@
 package common.gradle.scripts
 
 apply(from = "${rootProject.projectDir}/buildSrc/versions.gradle.kts") // parent properties
-apply(from = "${rootProject.projectDir}/versions.gradle.kts") // can override "parent" properties
+if (File("${rootProject.projectDir}/versions.gradle.kts").exists()) {
+    apply(from = "${rootProject.projectDir}/versions.gradle.kts") // can override "parent" properties
+}
+if (File("${project.projectDir}/versions.gradle.kts").exists()) {
+    apply(from = "${project.projectDir}/versions.gradle.kts") // can override "rootProject" properties
+}
